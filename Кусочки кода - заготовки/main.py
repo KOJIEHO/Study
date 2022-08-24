d11b2909431846e3c4fb5335373df87c31ae67db
KOJIEHO <75936661+KOJIEHO@users.noreply.github.com>
2022-08-24 19:40:10 +0300

    Create main.py

---
+++ Кусочки кода - заготовки/main.py
@@ -0,0 +1,27 @@
+###### Сохранение фото из интернета ######
+Photo = requests.get('Адресс страницы')
+Photo_option = open('Название фото', 'wb')
+Photo_option.write(Photo.content)
+Photo_option.close()
+
+
+###### Открытие и запись данных в csv файл ######
+# Открытие и запись названий столбцов в csv файл #
+with open("Data1.csv", "a", newline="") as file:
+    writer = csv.writer(file, delimiter=";")
+    writer.writerow(
+        ["Столбец №1", "Столбец №2", "MСтолбец №3", ..., "Столбец №"]
+    )
+# Открытие и запись данных в каждый столбец в csv файл #
+with open("Data1.csv", "a", encoding='utf-16', newline="") as file:
+    writer = csv.writer(file, delimiter=";")
+    writer.writerow(
+        [Данные для столбца №1, Данные для столбца №2, Данные для столбца №3, ..., Данные для столбца №N]
+    )
+    
+   
+###### Создние БД, запись данных в БД, выгрузка данных из БД ######
+base.execute('CREATE TABLE IF NOT EXISTS Название Таблицы(Название столбца №1, Название столбца №2, Название столбца №3, ..., Название столбца №N")')
+base.commit()
+cur.execute('INSERT INTO TABLE1 VALUES(?, ?, ..., N)', (Данные для столбца №1, Данные для столбца №2, Данные для столбца №3, ..., Данные для столбца №N))
+base.commit()
