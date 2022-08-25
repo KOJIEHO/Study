de059f73ae6458fd5159b7a2a693f98c23f107fb
KOJIEHO <75936661+KOJIEHO@users.noreply.github.com>
2022-08-25 21:06:15 +0300

    Update main.py

--- Кусочки кода - заготовки/main.py
+++ Кусочки кода - заготовки/main.py
@@ -23,8 +23,14 @@ with open("Data1.csv", "a", encoding='utf-16', newline="") as file:
 ###### Создние БД, запись данных в БД, выгрузка данных из БД ######
 base = sqlite3.connect('Название БД.db')
 cur = base.cursor()
+
 base.execute('CREATE TABLE IF NOT EXISTS Название Таблицы(Название столбца №1, Название столбца №2, Название столбца №3, ..., Название столбца №N")')
 base.commit()
+
 cur.execute('INSERT INTO Название Таблицы VALUES(?, ?, ..., N)', (Данные для столбца №1, Данные для столбца №2, Данные для столбца №3, ..., Данные для столбца №N))
 base.commit()
+
 info = cur.execute('SELECT Название Столбца FROM Название Таблицы').fetchall()
+
+cur.execute('DELETE FROM Название Таблицы')
+base.commit()
